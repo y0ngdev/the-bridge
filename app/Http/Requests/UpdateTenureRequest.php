a1c45f6e21ab591fbf3c4533cd844a5149c529cc
Prosper Ugbovo <prosperugbovo@gmail.com>
2025-12-21 19:01:03 +0100

    feat: manage tenure

---
+++ app/Http/Requests/UpdateTenureRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTenureRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'year' => ['required', 'string', 'max:20', Rule::unique('tenures', 'year')->ignore($this->route('tenure'))],
+        ];
+    }
+}
